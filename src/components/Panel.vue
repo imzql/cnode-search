41d2d590a715284f488be2352a38cb129b0ec5c8
小赵同学 <94612053+imzql@users.noreply.github.com>
2024-05-20 18:38:28 +0800

    功能基本做完,2个bug还没解决

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,13 +1,14 @@
 <template>
-    <div class="weui-panel weui-panel_access">
-    <div v-for="(n,index) in newComputed" :key="index" class="weui-panel__bd">
+  <div class="weui-panel weui-panel_access">
+    <div v-for="(n, index) in newComputed" :key="index" class="weui-panel__bd">
       <a href="javascript:void(0);" class="weui-media-box weui-media-box_appmsg">
         <div class="weui-media-box__hd">
-          <img class="weui-media-box__thumb" :src="n.author.avatar_url" alt />
+          <img class="weui-media-box__thumb" :src="n.author.avatar_url" />
         </div>
         <div class="weui-media-box__bd">
           <h4 class="weui-media-box__title" v-text="n.title"></h4>
           <p class="weui-media-box__desc" v-text="n.author.loginname"></p>
+          <span class="weui-media-box__desc" v-text="dayjs(n.create_at).format('YYYY-M-D')"></span>
         </div>
       </a>
     </div>
@@ -21,33 +22,50 @@
 </template>
 
 <script setup lang="ts">
-import {ref,reactive, computed} from 'vue'
+import { ref, reactive, computed, onMounted } from 'vue'
 import { useDataStore } from '@/stores/useData'
 import axios from 'axios'
+import dayjs from 'dayjs'
 //接收pinia传来的数据
 let dataStore = useDataStore()
 //数据
 let page = ref(1)
-let news = ref([])
-let newComputed = computed(() => {})
+let news: any = ref([])
+let newComputed = computed(() => {
+  if (dataStore.searchValue) {
+    return news.value.filter((item: any) => {
+      console.log(item)
+      
+      if (item.title.findIndex(dataStore.searchValue) != -1) {
+        return item
+      }
+    })
+  } else {
+    return news.value
+  }
+})
 let searchValue = dataStore.searchValue
 //封装函数 loadMore
-async function loadMore(){
-    let data = await axios({
-    url:'https://cnodejs.org/api/v1/topics',
-    params:{
-        limit:10,
-        page
-    }})
-    console.log(data);
-    
+async function loadMore() {
+  let data = await axios({
+    url: 'https://cnodejs.org/api/v1/topics',
+    params: {
+      limit: 10,
+      page:page.value
+    }
+  })
+  console.log(data);
+  //叠加页数
+  page.value += 1
+  news.value = [...news.value, ...data.data.data]
 }
 
-// ajax请求数据
+// 挂载钩子
+onMounted(() => {
+  loadMore()
+})
 
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
